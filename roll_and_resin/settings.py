90cc426a99c5859fba9ba5828ac9e7db25795815
Melody Shutt <melody.lisa1990@gmail.com>
2024-04-16 13:03:53 +0000

    Refactor allowed hosts and debug

--- roll_and_resin/settings.py
+++ roll_and_resin/settings.py
@@ -26,16 +26,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = development
+DEBUG = 'DEVELOPMENT' in os.environ
 
-if development:
-    ALLOWED_HOSTS = ['8000-melodylisa-rollresin-1cqbigpy9ih.ws-eu110.gitpod.io']
-else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
-
+ALLOWED_HOSTS = [
+    '8000-melodylisa-rollresin-1cqbigpy9ih.ws-eu110.gitpod.io',
+    'https://roll-and-resin-468ea4759605.herokuapp.com']
 
 # Application definition
 
