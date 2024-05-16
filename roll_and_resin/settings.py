072d70fe6a9c63fa4b59f646a414ab2f99f185d9
Melody Shutt <melody.lisa1990@gmail.com>
2024-05-16 09:55:21 +0000

    Add bag and delivery contexts

--- roll_and_resin/settings.py
+++ roll_and_resin/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -169,3 +170,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Delivery information
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
