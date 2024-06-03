c6027d9e458aef53d0f94a7b1c3062ab30c90d34
Melody Shutt <melody.lisa1990@gmail.com>
2024-06-03 11:48:15 +0000

    Fix issue with stripe payments

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,6 +1,6 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -22,6 +22,7 @@ var style = {
         iconColor: 'red'
     }
 };
+
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
--- checkout/views.py
+++ checkout/views.py
@@ -31,13 +31,13 @@ def checkout(request):
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
-            for item_id, quantity in bag.items():
+            for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
                     order_line_item = OrderLineItem(
                         order=order,
                         product=product,
-                        quantity=quantity,
+                        quantity=item_data,
                     )
                     order_line_item.save()
                 except Product.DoesNotExist:
