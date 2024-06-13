faccc30f2d010a0c349f91a324d842c5510a928f
Melody Shutt <melody.lisa1990@gmail.com>
2024-06-13 11:39:49 +0000

    Add wishlist admin and models

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Favourites
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    """
+    Admin class for the wishlist model.
+    """
+    list_display = (
+        'username',
+    )
+    search_fields = (
+        'username',
+    )
+    list_filter = (
+        'username',
+    )
+    list_per_page = 20
+
+
+admin.site.register(Wishlist, WishlistAdmin)
