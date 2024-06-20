d708112019ffb4d2f2bd8e25496bff73ffee481d
Melody Shutt <melody.lisa1990@gmail.com>
2024-06-20 13:24:58 +0000

    Fix issue with code refactor

--- roll_and_resin/settings.py
+++ roll_and_resin/settings.py
@@ -126,23 +126,19 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'UserAttributeSimilarityValidator'
-        )},
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.MinimumLengthValidator',
-        )},
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.CommonPasswordValidator',
-        )},
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
     {
-        'NAME': (
-            'django.contrib.auth.password_validation.NumericPasswordValidator',
-            )},
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
+
 
 
 # Internationalization
