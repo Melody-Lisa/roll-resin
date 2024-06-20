068aa49a2c7d13963a8dc05fa216f3efd18e44ba
Melody Shutt <melody.lisa1990@gmail.com>
2024-06-20 13:10:42 +0000

    Improve urls formatting

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -151,7 +151,6 @@ class StripeWH_Handler:
             status=200
         )
 
-
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
--- news/urls.py
+++ news/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('', views.news_items, name='news_items'),
     path('add_news_item/', views.add_news_item, name='add_news_item'),
     path('edit_news_item/<int:news_item_id>/', views.edit_news_item,
-         name='edit_news_item'),
+           name='edit_news_item'),
     path('delete_news_item/<int:news_item_id>/', views.delete_news_item,
-         name='delete_news_item'),
+           name='delete_news_item'),
     path('<int:news_item_id>/', views.news_item, name='news_item'),
 ]
